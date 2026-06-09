875f1c80c4e687de11a593426edaca852fc23904
Swethakit <kit27.ad56@gmail.com>
2026-06-09 13:53:05 +0530

    Revert backend commit from main

--- agents/investigation_agent.py
+++
@@ -1,5 +0,0 @@
-def investigate_incident():
-    return {
-        "severity": "Critical",
-        "root_cause": "Database Timeout caused by DB Pool update"
-    }
--- agents/timeline_agent.py
+++
@@ -1,7 +0,0 @@
-def get_timeline():
-    return [
-        "10:00 Deployment",
-        "10:02 Error Spike",
-        "10:04 Service Down",
-        "10:08 Recovery"
-    ]
